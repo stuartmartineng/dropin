d1ad79c7141e96977e9716cb61359eea21d8ecd2
unknown <stirtfmartin@gmail.com>
2020-02-17 15:24:15 -0500

    Added Unit Tests

--- apps/leagues/tests.py
+++ apps/leagues/tests.py
@@ -1,3 +1,34 @@
-from django.test import TestCase
-
 # Create your tests here.
+from django.test import TestCase
+from .models import League
+from users.models import User
+from django.urls import reverse
+from django.test import Client
+
+class LeagueTest(TestCase):
+    def createUser(self):
+        return User.objects.create(username="Test")
+    #Models#
+    def test_create_league(self):
+        user = self.createUser()
+        league = League.objects.create(name="Test League", owner=user)
+        self.assertTrue(isinstance(league, League))
+        self.assertEqual(league.__str__(), league.name)
+
+    #Views#
+    def test_new_league(self):
+        url = reverse("leagues:newLeague")
+        resp = self.client.get(url)
+
+        self.assertEqual(resp.status_code, 200)
+
+    def test_save_league(self):
+        c = Client()
+        user = self.createUser()
+        user.set_password('password')
+        user.save()
+        c.login(username="Test", password="password")
+        response = c.post(reverse('leagues:createLeague'), {'leagueName': 'testLeague'})
+
+        testLeague = League.objects.get(name="testLeague")
+        self.assertEqual(testLeague.name, 'testLeague')
--- apps/main/tests.py
+++ apps/main/tests.py
@@ -3,4 +3,29 @@ from __future__ import unicode_literals
 
 from django.test import TestCase
 
+from leagues.models import League
+from users.models import User
+from django.test import Client
+from django.urls import reverse
+
+
 # Create your tests here.
+class MainTest(TestCase):
+    #Views#
+    def createUser(self):
+        return User.objects.create(username="Test")
+
+    def test_home_page(self):
+        c = Client()
+        user = self.createUser()
+        user.set_password('password')
+        user.save()
+        c.login(username="Test", password="password")
+
+        league = League.objects.create(name="TestLeague", owner=user)
+        league.save()
+
+        url = reverse("main:index")
+        resp = c.get(url)
+        self.assertEqual(resp.status_code, 200)
+        self.assertIn(league.name.encode(), resp.content)
